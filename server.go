30518d4209ecc022841934a777ebb12ccbb3df14
zoe-gonzales <zoegonzales03@gmail.com>
2019-08-22 07:58:22 -0600

    adding initial server set up with echo, creating go.mod file

--- server.go
+++ server.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/labstack/echo"
 )
 
 func main() {
@@ -12,5 +15,12 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading env file")
 	}
-	// apiKey := os.Getenv("KEY")
+	apiKey := os.Getenv("KEY")
+	t := "I am happy"
+	getEmotions(apiKey, t)
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Good evening Ms. Gonzales, everything is working as expected")
+	})
+	e.Logger.Fatal(e.Start(":1333"))
 }
