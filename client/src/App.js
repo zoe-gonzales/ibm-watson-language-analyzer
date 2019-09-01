05aa342777f4c0c317e6c57d86bd557deb81c176
zoe-gonzales <zoegonzales03@gmail.com>
2019-09-01 11:11:27 -0600

    move Home component into components directory since it is the only item under pages directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Home from "./pages/Home";
+import Home from "./components/Home";
 
 function App() {
   return <Home apiResults={{}} />;
--- client/src/test/Home.test.js
+++ client/src/test/Home.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Home from '../pages/Home';
+import Home from '../components/Home';
 import renderer from 'react-test-renderer';
 import sinon from 'sinon';
 
