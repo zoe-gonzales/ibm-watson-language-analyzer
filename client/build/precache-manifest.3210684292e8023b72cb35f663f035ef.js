07923f54cd713f8adb4794faa3c5f03fa9bee32f
zoe-gonzales <zoegonzales03@gmail.com>
2019-09-02 17:25:49 -0600

    deploy to heroku

---
+++ client/build/precache-manifest.3210684292e8023b72cb35f663f035ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3c5ebf086240c2c1cda90a799c4686",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d0d8d2f93abd00444ff",
+    "url": "/static/css/main.1b3eea13.chunk.css"
+  },
+  {
+    "revision": "0e10ac4a2acdf259455e",
+    "url": "/static/js/2.8f3ac0b5.chunk.js"
+  },
+  {
+    "revision": "5d0d8d2f93abd00444ff",
+    "url": "/static/js/main.86cf9c71.chunk.js"
+  },
+  {
+    "revision": "db6be01aac7cb1c6d4ff",
+    "url": "/static/js/runtime~main.0033641b.js"
+  }
+]);
